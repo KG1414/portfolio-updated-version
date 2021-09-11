8e723942c62f0e636f62c413ff7141861a43b65e
kylegallard <kylegallard@gmail.com>
2021-09-11 19:47:57 +1000

    Few adjustments to project

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require("express");
 const request = require("request");
 const router = express.Router();
@@ -9,10 +10,14 @@ const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
 
 const contactEmail = nodemailer.createTransport({
     host: 'smtp.gmail.com',
