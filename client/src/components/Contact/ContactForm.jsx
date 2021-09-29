c73e294bc8d94bebf957e87c0f0ebebea5838762
kylegallard <kylegallard@gmail.com>
2021-09-29 22:31:09 +1000

    Test

--- client/src/components/Contact/ContactForm.jsx
+++ client/src/components/Contact/ContactForm.jsx
@@ -17,7 +17,7 @@ const ContactForm = () => {
 
         console.log(details);
 
-        let response = await fetch("http://localhost:5000/contact", {
+        let response = await fetch("https://kylegallard.herokuapp.com/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
